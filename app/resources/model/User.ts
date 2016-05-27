b4f921eb4daae593f1eb048abdd1f2b9afc06e0a
aaronpmishkin <aaronpmishkin@gmail.com>
2016-05-27 14:37:44 -0700

    Complete implementation of User class.
    Note that the user test are complete and were accidentally in commit: https://github.com/ValueChart/WebValueCharts/commit/8aa24c9d309abb47238c65e5211e5feaccd9ac81

--- app/resources/model/User.ts
+++ app/resources/model/User.ts
@@ -2,20 +2,44 @@
 * @Author: aaronpmishkin
 * @Date:   2016-05-25 16:41:35
 * @Last Modified by:   aaronpmishkin
-* @Last Modified time: 2016-05-25 16:56:01
+* @Last Modified time: 2016-05-27 10:28:21
 */
 
+import { WeightMap } 			from './WeightMap';
+import { ValueFunctionMap }		from './ValueFunctionMap';
 
 
 export class User {
 
 	private username: string;
-
-	// TODO: Add WeightMap, and ValueFunctionMap fields
+	private weightMap: WeightMap; 
+	private valueFunctionMap: ValueFunctionMap; 
 
 	constructor(username: string) {
 		this.username = username;
 	}
 
-	// TODO: Complete implementation of class.
+	getUsername(): string {
+		return this.username;
+	}
+
+	setUsername(username: string): void {
+		this.username = username;
+	}
+
+	getValueFunctionMap(): ValueFunctionMap {
+		return this.valueFunctionMap;
+	}
+
+	setValueFunctionMap(valueFunctionMap: ValueFunctionMap): void {
+		this.valueFunctionMap = valueFunctionMap;
+	}
+
+	getWeightMap(): WeightMap {
+		return this.weightMap;
+	}
+
+	setWeightMap(weightMap: WeightMap): void {
+		this.weightMap = weightMap;
+	}
 }
