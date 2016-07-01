3229e5e050ac948af546ddb08bd3f107f6a79684
Emily Hindalong <ehindalong@gmail.com>
2016-06-30 23:52:43 -0700

    Fixing minor merge error

--- app/resources/components/createValueChart-component/CreateValueChart.component.ts
+++ app/resources/components/createValueChart-component/CreateValueChart.component.ts
@@ -14,7 +14,7 @@ import { ValueChart } 	from '../../model/ValueChart';
 @Component({
 	selector: 'createValueChart',
 	templateUrl: 'app/resources/components/createValueChart-component/CreateValueChart.template.html',
-	directives: [ROUTER_DIRECTIVES]
+	directives: [ROUTER_DIRECTIVES],
 	providers: [CreationStepsService]
 })
 export class CreateValueChartComponent implements OnInit {
