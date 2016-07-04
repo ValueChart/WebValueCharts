6c67c24083c472a7220b1f6bc726ba350eab84b1
aaronpmishkin <aaronpmishkin@gmail.com>
2016-07-04 16:09:11 -0700

    Move index file to project root directory. This solves an issue with deploying the project.

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {
     middleware: {
       // overrides the second middleware default with new settings
-      1: require('connect-history-api-fallback')({index: './app/index.html', verbose: true})
+      1: require('connect-history-api-fallback')({index: './index.html', verbose: true})
     }
   }
 };
