618e2a56443f46a1b6f17c84c598f793d807580d
aaronpmishkin <aaronpmishkin@gmail.com>
2016-07-28 21:02:17 -0700

    Remove line of testing code from ValueChartViewerComponent that was accidently left in the class.

--- app/resources/components/valueChartViewer-component/ValueChartViewer.component.ts
+++ app/resources/components/valueChartViewer-component/ValueChartViewer.component.ts
@@ -2,7 +2,7 @@
 * @Author: aaronpmishkin
 * @Date:   2016-06-03 10:00:29
 * @Last Modified by:   aaronpmishkin
-* @Last Modified time: 2016-07-28 16:52:16
+* @Last Modified time: 2016-07-28 21:02:10
 */
 
 import { Component }															from '@angular/core';
@@ -167,8 +167,6 @@ export class ValueChartViewerComponent implements OnInit {
 			}
 
 		});
-
-		this.groupVcHttpService.createGroupValueChart(this.valueChart, 'ThisIsATestPassword').subscribe( valuechart => console.log(valuechart));
 
 		this.resizeValueChart();
 		this.$ = $;
