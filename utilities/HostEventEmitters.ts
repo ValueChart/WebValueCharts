127a21f196a6c2495c58be262b0aa9e5e40f3633
aaronpmishkin <aaronpmishkin@gmail.com>
2016-08-19 12:48:48 -0700

    Create potential fix for issues with joining ValueCharts.

--- utilities/HostEventEmitters.ts
+++ utilities/HostEventEmitters.ts
@@ -2,17 +2,18 @@
 * @Author: aaronpmishkin
 * @Date:   2016-07-29 16:32:51
 * @Last Modified by:   aaronpmishkin
-* @Last Modified time: 2016-07-29 20:50:38
+* @Last Modified time: 2016-08-19 12:47:14
 */
 
 
 import * as events from 'events';
 
-export class HostEventEmitter extends events.EventEmitter { 
+export class HostEventEmitter extends events.EventEmitter {
 	static USER_ADDED_EVENT: string = 'userAdded';
 	static USER_REMOVED_EVENT: string = 'userRemoved';
 	static USER_CHANGED_EVENT: string = 'userChanged';
 };
 
-export const hostEventEmitter = new HostEventEmitter(); 
+
+export const hostEventEmitter: HostEventEmitter = new HostEventEmitter();
 
