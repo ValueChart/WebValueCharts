ValueChart/WebValueCharts

commit 70cc704e4f926b625d6523d7f9b4160f3e87f1b4

This commit has the following changes: (1) Implement group/individual distinction in create workflow with simple radio button toggle; warn users before converting group chart to individual chart when there are other users in it; configure workflow buttons based on chart type (2) Hide Users tab and lock/unlock button when base chart is individual; hide checkbox/remove/colorkey when active chart is individual (3) Simplify mechanism for switching between charts - specifically, make it so that the active chart is only used by ValueChartViewer and its dependents, and use the base chart everywhere else in the application. This eliminates the need to set the active chart type outside of the viewer (which was the source of at least one bug). Change a couple ValueChartService field and methods names to improve clarity and transparency. (4) Bug fixes: 	(a) Remove code from ValueChart.ts methods that overwrote the chart type (this is no longer needed now that the type is defined by the creator) 	(b) Set userRole to undefined on log-out 	(c) Set chart type to individual before checking for structural changes in ValueChartViewer/HostService (otherwise the comparison will always fail) (5) Other miscellaneous clean-up and code reorganization throughout

client/resources/modules/app/components/Home/Home.component.ts
@@ -17,7 +17,7 @@ import { ValueChartHttpService }						from '../../services/ValueChartHttp.servic
 import { ValidationService }							from '../../services/Validation.service';
 
 // Import Model Classes:
-import { ValueChart }									from '../../../../model/ValueChart';
+import { ValueChart, ChartType }						from '../../../../model/ValueChart';
 
 // Import Utility Classes:
 import * as Formatter									from '../../../utilities/classes/Formatter';
@@ -117,12 +117,15 @@ export class HomeComponent {
 		this.valueChartHttpService.getValueChart(Formatter.nameToID(chartName), chartPassword)
 			.subscribe(
 			(valueChart: ValueChart) => {
-				this.valueChartService.setValueChart(valueChart);
-				this.valueChartService.setActiveChart('individual');
-				
-				this.currentUserService.setUserRole(UserRole.Participant);
 				$('#chart-credentials-modal').modal('hide');
-				this.router.navigate(['createValueChart/newUser/ScoreFunctions']);
+				if (valueChart.isIndividual()) {
+					toastr.error("The chart you are trying to join is single-user only.")
+				}
+				else {
+					this.valueChartService.setBaseValueChart(valueChart);	
+					this.currentUserService.setUserRole(UserRole.Participant);
+					this.router.navigate(['createValueChart/newUser/ScoreFunctions']);
+				}
 			},
 			// Handle Server Errors (like not finding the ValueChart)
 			(error) => {
@@ -142,11 +145,10 @@ export class HomeComponent {
 		this.valueChartHttpService.getValueChartByName(Formatter.nameToID(chartName), chartPassword)
 			.subscribe(
 			(valueChart: ValueChart) => {
-				this.valueChartService.setValueChart(valueChart);
-				this.currentUserService.setUserRole(UserRole.Viewer);
 				$('#chart-credentials-modal').modal('hide');
-				if (this.validateChartStructure()) {
-					let valueChart = this.valueChartService.getValueChart();
+				if (this.validateChartStructure(valueChart)) {
+					this.valueChartService.setBaseValueChart(valueChart);
+					this.currentUserService.setUserRole(UserRole.Viewer);
 					this.router.navigate(['view', 'group', valueChart.getFName()], { queryParams: { password: valueChart.password } });
 				}
 			},
@@ -164,9 +166,9 @@ export class HomeComponent {
 						This method will be removed when demonstration charts are removed from the home page.
 	*/
 	selectDemoValueChart(demoChart: any): void {
-		this.valueChartService.setValueChart(this.valueChartParser.parseValueChart(demoChart.xmlString));
+		let valueChart = this.valueChartParser.parseValueChart(demoChart.xmlString);
+		this.valueChartService.setBaseValueChart(valueChart);
 		this.currentUserService.setUserRole(UserRole.Participant);
-		let valueChart = this.valueChartService.getValueChart();
 		let viewType = valueChart.isIndividual() ? 'individual' : 'group';
 		this.router.navigate(['view', viewType, valueChart.getFName()], { queryParams: { password: valueChart.password } });
 	}
@@ -186,21 +188,20 @@ export class HomeComponent {
 		reader.onload = (fileReaderEvent: ProgressEvent) => {
 			if (event.isTrusted) {
 				var xmlString = (<FileReader>fileReaderEvent.target).result;	// Retrieve the file contents string from the file reader.
-				// Parse the XML string and set it to be the ValueChartService's active chart.
-				this.valueChartService.setValueChart(this.valueChartParser.parseValueChart(xmlString));
-				
-				let role: UserRole = UserRole.Viewer;
-				// The user uploaded a ValueChart so they aren't joining an existing one.
-				if (this.valueChartService.currentUserIsMember())
-					role = UserRole.OwnerAndParticipant;
-				else 
-					role = UserRole.Owner;
-				this.currentUserService.setUserRole(role)
+				let valueChart = this.valueChartParser.parseValueChart(xmlString);
 
-				if (this.validateChartStructure()) {
+				if (this.validateChartStructure(valueChart)) {
+					this.valueChartService.setBaseValueChart(valueChart);
+					
+					let role: UserRole = UserRole.Viewer;
+					if (this.valueChartService.currentUserIsMember())
+						role = UserRole.OwnerAndParticipant;
+					else 
+						role = UserRole.Owner;
+					this.currentUserService.setUserRole(role);
+
 					// Navigate to the ValueChartViewerComponent to display the ValueChart.
-					this.saveValueChartToDatabase(this.valueChartService.getValueChart());
-					let valueChart = this.valueChartService.getValueChart();
+					this.saveValueChartToDatabase(valueChart);
 					let viewType = valueChart.isIndividual() ? 'individual' : 'group';
 					this.router.navigate(['view', viewType, valueChart.getFName()], { queryParams: { password: valueChart.password } });
 				}		
@@ -217,8 +218,8 @@ export class HomeComponent {
 		@description 	Validates chart structure prior to viewing and gives the creator an opportunity to fix errors.
 						Returns true iff there were no validation errors.
 	*/
-	validateChartStructure(): boolean {
-		let structuralErrors = this.validationService.validateStructure(this.valueChartService.getValueChart()); 	
+	validateChartStructure(valueChart: ValueChart): boolean {
+		let structuralErrors = this.validationService.validateStructure(this.valueChartService.getBaseValueChart()); 	
 		if (structuralErrors.length > 0) {
 			if (this.valueChartService.currentUserIsCreator()) {
 				this.canFixChart = false;

client/resources/model/ValueChart.ts
@@ -243,9 +243,9 @@ export class ValueChart {
 		this.alternatives = alternatives;
 	}
 
-	addAlternative(alernative: Alternative): void {
-		if (this.alternatives.indexOf(alernative) === -1) {
-			this.alternatives.push(alernative);
+	addAlternative(alternative: Alternative): void {
+		if (this.alternatives.indexOf(alternative) === -1) {
+			this.alternatives.push(alternative);
 		}
 	}
 
@@ -280,11 +280,6 @@ export class ValueChart {
 
 	setUsers(users: User[]): void {
 		this.users = users;
-
-		if (this.users.length > 1) 
-			this.type = ChartType.Group;
-		else 
-			this.type = ChartType.Individual;
 	}
 
 	setUser(newUser: User): void {
@@ -297,11 +292,6 @@ export class ValueChart {
 		} else {
 			this.users[userIndex] = newUser;
 		}
-
-		if (this.users.length > 1) 
-			this.type = ChartType.Group;
-		else 
-			this.type = ChartType.Individual;
 	}
 
 	removeUser(userToDelete: User): void {
@@ -312,11 +302,6 @@ export class ValueChart {
 		if (index !== -1) {
 			this.users.splice(index, 1);
 		}
-
-		if (this.users.length > 1) 
-			this.type = ChartType.Group;
-		else 
-			this.type = ChartType.Individual;
 	}
 
 	/*

client/resources/modules/app/components/DetailBox/DetailBox.component.ts
@@ -18,7 +18,7 @@ import { ObjectiveChartDefinitions }											from '../../../ValueChart/definit
 
 import { ValueChartHttpService }												from '../../services/ValueChartHttp.service';
 import { HostService }															from '../../services/Host.service';
-import { DisplayedUsersService }													from '../../services/DisplayedUsers.service';
+import { DisplayedUsersService }												from '../../services/DisplayedUsers.service';
 import { RenderEventsService }													from '../../../ValueChart/services/RenderEvents.service';
 
 
@@ -49,6 +49,7 @@ export class DetailBoxComponent implements OnInit {
 	@Input() chartElement: d3.Selection<any, any, any, any>;
 	@Input() enableManagement: boolean
 	@Input() viewConfig: ViewConfig;
+	@Input() showUsers: boolean;
 	@Input() width: number;
 	@Input() height: number;
 

client/resources/modules/app/components/DetailBox/DetailBox.template.html
@@ -12,7 +12,7 @@
 			<h5 id="alternative-header">{{detailBoxAlternativeTab}}</h5>
 			</button>
 		</div>
-		<div class="form-group valuechart-toolbar-group pull-left">
+		<div *ngIf="showUsers" class="form-group valuechart-toolbar-group pull-left">
 			<button class="btn btn-lg btn-default detail-box-tab detail-box-header" id="user-list-header-container"
 			(click)="detailBoxCurrentTab = DETAIL_BOX_USERS_TAB" [class.active]="detailBoxCurrentTab === DETAIL_BOX_USERS_TAB">
 			<h5 id="user-list-headers">User List</h5>
@@ -64,9 +64,11 @@
 			<div *ngFor="let user of valueChart.getUsers(); let i = index ">
 				<h5 class="remove-user">
 					{{user.getUsername()}}
-					<span class="pull-right"><input type="color" value="{{user.color}}" (change)="setUserColor(user, $event.target.value)"></span>
-					<button *ngIf="enableManagement" class="pull-right btn btn-danger btn-xxs" (click)="userToRemove=user" data-toggle="modal" data-target="#remove-user-modal">Remove</button>
-					<input class="pull-right" type="checkbox" [disabled]="displayedUsersService.isUserInvalid(user.getUsername())" [checked]="displayedUsersService.isUserDisplayed(user)" (change)="changeDisplayedUsers(user, $event)">
+					<div *ngIf="!valueChart.isIndividual()">
+						<span class="pull-right"><input type="color" value="{{user.color}}" (change)="setUserColor(user, $event.target.value)"></span>
+						<button *ngIf="enableManagement" class="pull-right btn btn-danger btn-xxs" (click)="userToRemove=user" data-toggle="modal" data-target="#remove-user-modal">Remove</button>
+						<input class="pull-right" type="checkbox" [disabled]="displayedUsersService.isUserInvalid(user.getUsername())" [checked]="displayedUsersService.isUserDisplayed(user)" (change)="changeDisplayedUsers(user, $event)">
+					</div>
 				</h5>
 			</div>
 		</div>

client/resources/modules/app/components/ExportValueChart/ExportValueChart.component.ts
@@ -27,7 +27,7 @@ import { ValueChart }										from '../../../../model/ValueChart';
 	template: `
 				<div class="export-value-chart">
 					<a class="btn btn-default" id="download-value-chart"
-						[class.disabled]="!valueChartService.getValueChart()" 
+						[class.disabled]="!valueChartService.getBaseValueChart()" 
 						download="{{getValueChartName()}}" 
 						href="javascript:void(0)" 
 						(click)="downloadValueChart()">
@@ -74,7 +74,7 @@ export class ExportValueChartComponent implements OnInit {
 	}
 
 	getValueChartName(): string {
-		var valueChart: ValueChart = this.valueChartService.getValueChart();
+		var valueChart: ValueChart = this.valueChartService.getBaseValueChart();
 
 		if (valueChart) {
 			return valueChart.getFName() + '.xml';
@@ -85,8 +85,7 @@ export class ExportValueChartComponent implements OnInit {
 	}
 
 	downloadValueChart(): void {
-		console.log('downloading chart');
-		var valueChart: ValueChart = this.valueChartService.getValueChart();
+		var valueChart: ValueChart = this.valueChartService.getBaseValueChart();
 		var valueChartObjectURL: string = this.convertValueChartIntoObjectURL(valueChart);
 
 		this.downloadLink.setAttribute('href', valueChartObjectURL);		// Set the download link on the <a> element to be the URL created for the XML string.

client/resources/modules/app/components/MyValueCharts/MyValueCharts.component.ts
@@ -108,13 +108,7 @@ export class MyValueChartsComponent implements OnInit {
 		
 		this.valueChartHttpService.getValueChart(chartId, password)
 			.subscribe(valueChart => {
-				this.valueChartService.setValueChart(valueChart);
 				
-				if (this.valueChartService.currentUserIsMember())
-					this.currentUserService.setUserRole(UserRole.OwnerAndParticipant);
-				else 
-					this.currentUserService.setUserRole(UserRole.Owner);
-
 				// Validate chart structure before proceeding.
 				// (This is a sanity check to catch any as any errors brought on by 
 				//  changes in validation since saving to the database.)
@@ -124,6 +118,8 @@ export class MyValueChartsComponent implements OnInit {
 					$('#validate-modal').modal('show');
 				}
 				else {
+					this.valueChartService.setBaseValueChart(valueChart);
+					this.currentUserService.setUserRole(this.valueChartService.currentUserIsMember() ? UserRole.OwnerAndParticipant : UserRole.Owner);
 					let viewType = valueChart.isIndividual() ? 'individual' : 'group';
 					this.router.navigate(['view', viewType, valueChart.getFName()], { queryParams: { password: valueChart.password } });
 				}	
@@ -135,11 +131,8 @@ export class MyValueChartsComponent implements OnInit {
 
 		this.valueChartHttpService.getValueChart(chartId, password)
 			.subscribe(valueChart => {
-				this.valueChartService.setValueChart(valueChart);
-				if (this.valueChartService.currentUserIsMember())
-					this.currentUserService.setUserRole(UserRole.OwnerAndParticipant);
-				else 
-					this.currentUserService.setUserRole(UserRole.Owner);
+				this.valueChartService.setBaseValueChart(valueChart);
+				this.currentUserService.setUserRole(this.valueChartService.currentUserIsMember() ? UserRole.OwnerAndParticipant : UserRole.Owner);
 
 				this.router.navigate(['/createValueChart/editChart/BasicInfo']);
 			});
@@ -148,8 +141,7 @@ export class MyValueChartsComponent implements OnInit {
 	editPreferences(chartId: string, chartName: string, password: string): void {
 		this.valueChartHttpService.getValueChart(Formatter.nameToID(chartName), password)
 			.subscribe(valueChart => {
-				this.valueChartService.setValueChart(valueChart);
-				this.valueChartService.setActiveChart('individual');
+				this.valueChartService.setBaseValueChart(valueChart);
 				this.currentUserService.setUserRole(UserRole.Participant);
 
 				this.router.navigate(['/createValueChart/editPreferences/ScoreFunctions']);
@@ -183,12 +175,12 @@ export class MyValueChartsComponent implements OnInit {
 	setValueChart(chartId: string, password: string): void {
 		this.valueChartHttpService.getValueChart(chartId, password)
 			.subscribe(valueChart => {
-				this.valueChartService.setValueChart(valueChart);
+				this.valueChartService.setBaseValueChart(valueChart);
 			});
 	}
 
 	getValueChartName(): string {
-		var valueChart: ValueChart = this.valueChartService.getValueChart();
+		var valueChart: ValueChart = this.valueChartService.getBaseValueChart();
 
 		if (valueChart) {
 			return valueChart.getFName() + 'UserWeights.csv';
@@ -198,7 +190,7 @@ export class MyValueChartsComponent implements OnInit {
 	}
 
 	exportUserWeights() {
-		var valueChart: ValueChart = this.valueChartService.getValueChart();
+		var valueChart: ValueChart = this.valueChartService.getBaseValueChart();
 		var weightsObjectUrl: string = this.convertUserWeightsIntoObjectURL(valueChart);
 
 		this.downloadLink.setAttribute('href', weightsObjectUrl);	// Set the download link on the <a> element to be the URL created for the CSV string.

client/resources/modules/app/components/MyValueCharts/MyValueCharts.template.html
@@ -97,7 +97,7 @@
                     </div>
                     <div class="row">
                         <div class="col-sm-offset-3 col-sm-9">
-                        	<button type="submit" class="btn btn-lg btn-success" (click)="deleteValueChart(valueChartService.getValueChart()._id, valueChartService.getValueChart().getFName())" data-dismiss="modal">OK</button>
+                        	<button type="submit" class="btn btn-lg btn-success" (click)="deleteValueChart(valueChartService.getBaseValueChart()._id, valueChartService.getBaseValueChart().getFName())" data-dismiss="modal">OK</button>
                             <button type="submit" class="btn btn-lg btn-danger" data-dismiss="modal">Cancel</button>
                         </div>
                     </div>
@@ -120,7 +120,7 @@
                     </div>
                     <div class="row">
                         <div class="col-sm-offset-3 col-sm-9">
-                        	<button type="submit" class="btn btn-lg btn-success" (click)="leaveChart(valueChartService.getValueChart()._id)" data-dismiss="modal">OK</button>
+                        	<button type="submit" class="btn btn-lg btn-success" (click)="leaveChart(valueChartService.getBaseValueChart()._id)" data-dismiss="modal">OK</button>
                             <button type="submit" class="btn btn-lg btn-danger" data-dismiss="modal">Cancel</button>
                         </div>
                     </div>

client/resources/modules/app/components/Root/Root.component.ts
@@ -115,11 +115,13 @@ export class RootComponent implements OnInit {
 				.subscribe(logoutResult => {
 					this.currentUserService.setLoggedIn(false);
 					this.currentUserService.setUsername(undefined);
+					this.currentUserService.setUserRole(undefined);
 					(<any> window).destination = '/register';
 					this.router.navigate(['/register']);
 				});
 		} else {
 			this.currentUserService.setUsername(undefined);
+			this.currentUserService.setUserRole(undefined);
 			(<any> window).destination = '/register';
 			this.router.navigate(['/register']);
 		}

commit 98ce9eca2635951d0badf0213c791ca00c6ffff3

Fix bug introduced by 70cc704e4f926b625d6523d7f9b4160f3e87f1b4 that broke viewing ValueCharts.

client/resources/modules/app/components/Home/Home.component.ts
@@ -2,7 +2,7 @@
 * @Author: aaronpmishkin
 * @Date:   2016-05-25 14:41:41
 * @Last Modified by:   aaronpmishkin
-* @Last Modified time: 2017-06-29 17:48:17
+* @Last Modified time: 2017-07-04 17:16:44
 */
 
 // Import Angular Classes:
@@ -145,11 +145,15 @@ export class HomeComponent {
 		this.valueChartHttpService.getValueChartByName(Formatter.nameToID(chartName), chartPassword)
 			.subscribe(
 			(valueChart: ValueChart) => {
+
 				$('#chart-credentials-modal').modal('hide');
 				if (this.validateChartStructure(valueChart)) {
 					this.valueChartService.setBaseValueChart(valueChart);
 					this.currentUserService.setUserRole(UserRole.Viewer);
-					this.router.navigate(['view', 'group', valueChart.getFName()], { queryParams: { password: valueChart.password } });
+
+					let viewType = valueChart.isIndividual() ? 'individual' : 'group';
+
+					this.router.navigate(['view', viewType, valueChart.getFName()], { queryParams: { password: valueChart.password } });
 				}
 			},
 			// Handle Server Errors (like not finding the ValueChart)
@@ -219,7 +223,7 @@ export class HomeComponent {
 						Returns true iff there were no validation errors.
 	*/
 	validateChartStructure(valueChart: ValueChart): boolean {
-		let structuralErrors = this.validationService.validateStructure(this.valueChartService.getBaseValueChart()); 	
+		let structuralErrors = this.validationService.validateStructure(valueChart); 	
 		if (structuralErrors.length > 0) {
 			if (this.valueChartService.currentUserIsCreator()) {
 				this.canFixChart = false;

test/client/e2e/viewExisting.test.ts
@@ -131,7 +131,7 @@ describe('View Existing Chart Page', () => {
 			let continueBtn = p.element.all(p.by.buttonText('Continue'));
 			continueBtn.click().then(function() {	
 				p.browser.waitForAngular();
-				expect(p.browser.getCurrentUrl()).to.eventually.equal('http://localhost:3000/view/group/ThisIsATestChart?password=temp');
+				expect(p.browser.getCurrentUrl()).to.eventually.equal('http://localhost:3000/view/individual/ThisIsATestChart?password=temp');
 			});
 
 	});
@@ -155,28 +155,29 @@ describe('View Existing Chart Page', () => {
 				var viewExistDialog = p.browser.element(p.by.cssContainingText('#modal-header', 'View Existing Chart'));
 				p.browser.sleep(1000);
 				expect(viewExistDialog.isDisplayed()).to.eventually.be.true;
-		});
+	
+				// Find page elements
+				var vcNameField = p.browser.element(p.By.id('chart-name-input'));
+				var passwordField = p.browser.element(p.By.id('chart-passsword-input'));
 
-			// Find page elements
-			var vcNameField = p.browser.element(p.By.id('chart-name-input'));
-			var passwordField = p.browser.element(p.By.id('chart-passsword-input'));
+				// Fill input keys
+				vcNameField.clear();
+				vcNameField.sendKeys('Cities');
+				passwordField.clear();
+				passwordField.sendKeys('australia');
 
-			// Fill input keys
-			vcNameField.clear();
-			vcNameField.sendKeys('Cities');
-			passwordField.clear();
-			passwordField.sendKeys('australia');
+				// Ensure fields contain what is entered
+				expect(vcNameField.getAttribute('value')).to.eventually.equal('Cities');
+				expect(passwordField.getAttribute('value')).to.eventually.equal('australia');
+				
+				// Click "Continue" to view value chart "Cities"
+				let continueBtn = p.element.all(p.by.buttonText('Continue'));
+				continueBtn.click().then(function() {
+					p.browser.waitForAngular();
+					expect(p.browser.getCurrentUrl()).to.eventually.equal('http://localhost:3000/view/group/Cities?password=australia');
+				});	
+			});
 
-			// Ensure fields contain what is entered
-			expect(vcNameField.getAttribute('value')).to.eventually.equal('Cities');
-			expect(passwordField.getAttribute('value')).to.eventually.equal('australia');
-			
-			// Click "Continue" to view value chart "Cities"
-			let continueBtn = p.element.all(p.by.buttonText('Continue'));
-			continueBtn.click().then(function() {
-				p.browser.waitForAngular();
-				expect(p.browser.getCurrentUrl()).to.eventually.equal('http://localhost:3000/view/group/Cities?password=australia');
-			});	
 	});
 
 	// The following test cases are to check whether a user can 